59785ab5abcde409a06d964fd3e7eb124e10b4d2
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2017-10-03 21:55:51 -0300

    v0.17.10.03

---
+++ application/views/pgdp/header/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<header>
+		<meta charset="utf-8">
+		<meta name="PGDP" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title><?php
+            echo msg('url_title');
+        ?></title>
+
+		<!-- Twitter -->
+		<meta name="twitter:site" content="@dpgp">
+		<meta name="twitter:creator" content="@dpgp">
+
+		<!-- Meta -->
+		<meta name="description" content="PGDP">
+		<meta name="author" content="Rene Faustino Gabriel junior <renefgj@gmail.com">
+
+		<!-- Bootstrap core CSS -->
+		<link href="<?php echo base_url('css/bootstrap.min.css'); ?>" rel="stylesheet">
+		<link href="<?php echo base_url('css/style.css'); ?>" rel="stylesheet">
+		
+		<!-- JS ------------------>
+		<script src="<?php echo base_url('js/utils.js'); ?>"></script>
+				
+		<script src="<?php echo base_url('js/jquery.min.js'); ?>"></script>				
+		
+		<script src="<?php echo base_url('js/tether.js'); ?>"></script>
+		<script src="<?php echo base_url('js/bootstrap.min.js'); ?>"></script>
+		
+		
+
+		<!-- Favicons -->
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		<link rel="icon" href="/favicon.ico">
+	</header>
+    
